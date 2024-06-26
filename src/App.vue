5a8611c4d5c988eeed893b1c95491a832addf176
Manas Gupta <manas94gupta@gmail.com>
2024-06-26 11:07:26 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.png" width="40" height="40" />
+  </header>
+
+  <main>Space Shooter Game</main>
+</template>
+
+<style scoped></style>
